4fe92360b21950ca04959ece4adc5d7e4b5fdede
Uri Miller <usm3361@gmail.com>
2026-01-15 12:58:29 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(7000);
+  console.log('Server running on http://localhost:3000');
 }
-bootstrap();
+
+void bootstrap();
